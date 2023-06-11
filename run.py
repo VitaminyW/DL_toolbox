38daa561008fcacb063bf82df773a4f38f1dc8e7
VitaminyW <1779723554@qq.com>
2023-06-11 15:16:14 +0800

    删除一些不必要的代码

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from pathlib import Path
 if __name__ == '__main__':
     import sys
     config_file = sys.argv[1]
-    # config_file = '/data/disk1/yewei/MICCA_cine/Code/script/Unet_3d.json'
     print(f'config_file is {config_file}')
     with open(config_file,'r') as f:
         config = json.load(f)
